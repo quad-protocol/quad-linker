f1d35c54c94fd7dd95f8243612b880abfb5b774f
xQuadDev <developer@quad.finance>
2020-11-09 22:35:21 +0100

    Need to deploy migrations regardless

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,11 +3,11 @@ const Migrations = artifacts.require("Migrations");
 const QuadAdmin = artifacts.require("QuadAdmin");
 
 module.exports = async function (deployer, network) {
+    await deployer.deploy(Migrations);
+
     //network test skips migrations
     if (network == "test")
         return;
-
-    await deployer.deploy(Migrations);
 
     await deployer.deploy(QuadAdmin);
 
